d5f4f676dbcbe28b62b8439d8f650243cdd86d26
SolaireLibrary <solairelibrary@mail.com>
2016-01-12 15:30:42 +0000

    BinToHexIStream
    Fixed bug in HexToBinIStream

--- Include/Solaire/Maths/Streams/HexIStream.hpp
+++ Include/Solaire/Maths/Streams/HexIStream.hpp
@@ -48,7 +48,7 @@ namespace Solaire {
             }else {
                 mStream >> buffer[1];
             }
-            return hexToBin8(buf);
+            return hexToBin8(buffer);
         }
 
         // Inherited from IStream
@@ -140,7 +140,7 @@ namespace Solaire {
 
     public:
         HexToBinIStream(IStream& aStream) :
-            msStream(aStream)
+            mStream(aStream)
         {}
 
         SOLAIRE_EXPORT_CALL ~HexToBinIStream() {
@@ -152,6 +152,148 @@ namespace Solaire {
         void SOLAIRE_EXPORT_CALL read(void* const aAddress, const uint32_t aBytes) throw() override {
             uint8_t* const ptr = static_cast<uint8_t*>(aAddress);
             for(uint32_t i = 0; i < aBytes; ++i) ptr[i] = readByte();
+        }
+
+        bool SOLAIRE_EXPORT_CALL isOffsetable() const throw() override {
+            return mStream.isOffsetable();
+        }
+
+        int32_t SOLAIRE_EXPORT_CALL getOffset() const throw() override {
+            return mStream.getOffset();
+        }
+
+        bool SOLAIRE_EXPORT_CALL setOffset(const int32_t aOffset) throw() override {
+            return mStream.setOffset(aOffset);
+        }
+
+        bool SOLAIRE_EXPORT_CALL end() const throw() override {
+            return mStream.end();
+        }
+
+    };
+
+    class BinToHexIStream : public IStream {
+    private:
+        IStream& mStream;
+        HexChar mBuffer;
+        bool mBufferSet;
+    private:
+        uint8_t readByte() throw() {
+            if(mBufferSet) {
+                mBufferSet = false;
+                return mBuffer;
+            }
+
+            HexChar buf[2];
+            uint8_t byte;
+            mStream >> byte;
+            bin8ToHex(byte, buf);
+            mBufferSet = true;
+            mBuffer = buf[1];
+            return buf[0];
+        }
+
+        // Inherited from IStream
+
+        uint8_t SOLAIRE_EXPORT_CALL readU8() throw() override {
+            return readByte();
+        }
+
+        uint16_t SOLAIRE_EXPORT_CALL readU16() throw() override {
+            return combine16(
+                readByte(),
+                readByte()
+            );
+        }
+
+        uint32_t SOLAIRE_EXPORT_CALL readU32() throw() override {
+            return combine32(
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte()
+            );
+        }
+
+        uint64_t SOLAIRE_EXPORT_CALL readU64() throw() override {
+            return combine64(
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte()
+            );
+        }
+
+        int8_t SOLAIRE_EXPORT_CALL readI8() throw() override {
+            return signCast(readByte());
+        }
+
+        int16_t SOLAIRE_EXPORT_CALL readI16() throw() override {
+            return signCast(combine16(
+                readByte(),
+                readByte()
+            ));
+        }
+
+        int32_t SOLAIRE_EXPORT_CALL readI32() throw() override {
+            return signCast(combine32(
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte()
+            ));
+        }
+
+        int64_t SOLAIRE_EXPORT_CALL readI64() throw() override {
+            return signCast(combine64(
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte(),
+                readByte()
+            ));
+        }
+
+        float SOLAIRE_EXPORT_CALL readF() throw() override {
+            float value;
+            read(&value, sizeof(float));
+            return value;
+        }
+
+        double SOLAIRE_EXPORT_CALL readD() throw() override {
+            double value;
+            read(&value, sizeof(double));
+            return value;
+        }
+
+        char SOLAIRE_EXPORT_CALL readC() throw() override {
+            return static_cast<char>(readByte());
+        }
+
+    public:
+        BinToHexIStream(IStream& aStream) :
+            mStream(aStream),
+            mBufferSet(false)
+        {}
+
+        SOLAIRE_EXPORT_CALL ~BinToHexIStream() {
+            //! \todo Handle trailing byte
+        }
+
+        // Inherited from IStream
+
+        void SOLAIRE_EXPORT_CALL read(void* const aAddress, const uint32_t aBytes) throw() override {
+            uint8_t* const ptr = static_cast<uint8_t*>(aAddress);
+            for(uint32_t i = 0; i < aBytes; ++i) {
+                ptr[i] = readByte();
+            }
         }
 
         bool SOLAIRE_EXPORT_CALL isOffsetable() const throw() override {
